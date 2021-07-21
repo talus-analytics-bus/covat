6ae592f63a6c998a2894648b51f4185c7e53b1ec
Ryan <rzimmerman@talusanalytics.com>
2021-07-21 12:08:46 -0600

    Triggering build with comment commit

--- src/html.js
+++ src/html.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+// triggering build with comment
+
 export default function HTML(props) {
   React.useEffect(() => {}, [])
   return (
